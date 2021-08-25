7edeb83ce5efd5f086f3129b4bdd55bd1786bca4
Joakim Stenmark <jocke.stenmark@live.se>
2021-08-25 09:24:42 +0200

    Finished dialogue system

--- Sammanslaget21_UnityProject/Assets/Scripts/DialogueSystem.cs
+++ Sammanslaget21_UnityProject/Assets/Scripts/DialogueSystem.cs
@@ -6,18 +6,16 @@ using UnityEngine.UI;
 using TMPro;
 
 
-
 public class DialogueSystem : MonoBehaviour
 {
     public static DialogueSystem instance;
 
     //Add queue variable
     private Queue<string> sentences;
+    private Queue<Monologue> dialogues;
 
-    public TextMeshProUGUI TextBox;
-    public TextMeshProUGUI NameBox;
-    
-
+    public TextMeshProUGUI speechBox;
+    public TextMeshProUGUI nameBox;
 
     private void Awake()
     {
@@ -29,25 +27,59 @@ public class DialogueSystem : MonoBehaviour
         {
             instance = this;
         }
+
+        sentences = new Queue<string>();
+        dialogues = new Queue<Monologue>();
+
+        sentences.Enqueue("if you read this something went wrong");
     }
 
     void Start()
     {
-        sentences = new Queue<string>();
     }
 
-    public void StartDialogue(Dialogue dialogue)
+
+    
+    public void StartDialogue(List<Monologue> newDialogues)
     {
         print("Starting New Dialogue");
+        
 
-        sentences.Clear();
-        if (dialogue.sentences.Length == 0)
+        dialogues.Clear();
+
+        if (newDialogues.Count == 0)
         {
-            Debug.LogWarning("No sentences in Dialogue");
+            Debug.LogWarning("No dialogues in newDialogues");
             return;
         }
 
-        foreach (string sentence in dialogue.sentences)
+        foreach (Monologue dialogue in newDialogues)
+        {
+            dialogues.Enqueue(dialogue);
+
+        }
+
+        //DisplayNextSentence();
+
+        StartMonologue(dialogues.Dequeue());
+    }
+
+    public void StartMonologue(Monologue monologue)
+    {
+        print("Starting New Monologue");
+
+    
+        sentences.Clear();
+
+        if (monologue.sentences.Length == 0)
+        {
+            Debug.LogWarning("No sentences in Monologue");
+            return;
+        }
+
+        nameBox.text = monologue.name;
+
+        foreach (string sentence in monologue.sentences)
         {
             sentences.Enqueue(sentence);
 
@@ -56,18 +88,25 @@ public class DialogueSystem : MonoBehaviour
         DisplayNextSentence();
     }
 
+    //This is mainly triggered by ContinueButton in the dialogue system prefab
     public void DisplayNextSentence()
     {
 
         if (sentences.Count == 0)
         {
-            print("Dialogue Ended");
-            TextBox.text = "";
+            if (dialogues.Count == 0)
+            {
+                print("All Dialogues done");
+
+  
+                return;
+            }
+
+            StartMonologue(dialogues.Dequeue());
             return;
         }
 
-        TextBox.text = sentences.Dequeue();
-
+        speechBox.text = sentences.Dequeue();
 
     }
 }
