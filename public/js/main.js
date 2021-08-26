0db9a4f3421eee0ee4efa134b2fe8452493cef62
gusjak <70512255+gusjak@users.noreply.github.com>
2021-08-26 20:47:02 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
-function startLoader() {
-    const preloader = document.querySelector(".preloader");
+// function startLoader() {
+//     const preloader = document.querySelector(".preloader");
 
-    window.addEventListener("DOMContentLoaded", function() {
-      setTimeout(function() {
-        preloader.style.visibility = "hidden";
-        document.body.classList.remove("loading");
-      }, 4000);
-    });
-  }
+//     window.addEventListener("DOMContentLoaded", function() {
+//       setTimeout(function() {
+//         preloader.style.visibility = "hidden";
+//         document.body.classList.remove("loading");
+//       }, 4000);
+//     });
+//   }
   
-  startLoader();
+//   startLoader();
   
